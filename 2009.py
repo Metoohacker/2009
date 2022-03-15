d5d17739736e7807b4f6b99288676f8aef08d03b
Metoohacker <90014946+Metoohacker@users.noreply.github.com>
2022-03-15 21:32:34 +0500

    2009.py

--- 2009.py
+++ 2009.py
@@ -113,11 +113,7 @@ if len(sys.argv) == 2:
 	if sys.argv[1] == "--info":
 		print("   _________________        \n  /  ___/\_   ___/        \n /   \  __ |    __)          \n \    \  \|     \ \033[0;96mGALAXY\033[0;97m        \n  \__  /\___  /__\033[0;96mFACEBOOK\033[0;97m_\n         \/     \/_____/____/")
 		print("\n [*] Author    :  "
-		print(" [ Sosial Medi  ] \n")
-		print(" [*] Facebook  : https://facebook.com/ ")
-		print(" [*] Instagram : https://instagram.com/ ")
-		print(" [*] YouTube   : https://youtube.com/ ")
-		exit(" [*] GitHub    : https://github.com/ ")
+		exit("\n [*] GitHub    : https://github.com/ ")
 	else:
 		Main()
 
